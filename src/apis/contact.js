badcce45cfb6afa421c7dfb409eb3b95a230a0bb
tanpm <tanpm@mobiletech.vn>
2022-02-18 21:12:46 +0700

    feat(contact): add flow create contact info api

---
+++ src/apis/contact.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { BASE_URL } from "./Api";
+
+export const getAll = (page, per_page, query = {}) => {
+  return axios({
+    url: `${BASE_URL}/api/contacts`,
+    method: "get",
+    params: {
+      page,
+      per_page,
+      ...query,
+    },
+  });
+};
+
+export const getById = (id) => {
+  return axios({
+    url: `${BASE_URL}/api/contacts/${id}`,
+    method: "get",
+  });
+};
+
+export const create = (contact) => {
+  return axios({
+    url: `${BASE_URL}/api/contacts/create`,
+    method: "post",
+    data: contact,
+  });
+};
+
+export const update = (id, contact) => {
+  return axios({
+    url: `${BASE_URL}/api/contacts/${id}`,
+    method: "put",
+    data: contact,
+  });
+};
+
+export const _delete = (id) => {
+  return axios({
+    url: `${BASE_URL}/api/contacts/${id}`,
+    method: "delete",
+  });
+};
